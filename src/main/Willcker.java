9efd943a577350c70d76b5e2bdb5f571e802de88
Willcker <fabiano.willcker@dcx.ufpb.br>
2021-11-15 22:32:41 -0300

    willcker soma

---
+++ src/main/Willcker.java
@@ -0,0 +1,12 @@
+public class Willcker {
+    private int primeiro ;
+    private int segundo ;
+    private int terceiro ;
+
+    public int soma (int primeiro, int segundo){
+        this.terceiro= primeiro + segundo ;
+        return terceiro;
+    }
+
+    
+}
--- src/main/java/br/ufpb/dcx/aps/atividades/a01/AloMundo.java
+++
@@ -1,7 +0,0 @@
-package br.ufpb.dcx.aps.atividades.a01;
-
-public class AloMundo {
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
-    }
-}
